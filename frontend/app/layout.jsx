0086793738f39728f28a244b8471d7ab8f566a1e
airwin <adrianirwin28@gmail.com>
2024-02-04 16:59:05 +0000

    Add Prettier + Update Next to 14.1.0

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,26 +1,24 @@
-import {Inter} from 'next/font/google'
-import './globals.css'
-import '@mantine/core/styles.css';
-import {ColorSchemeScript, MantineProvider} from '@mantine/core';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@mantine/core/styles.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Better DoneDeal Frontend',
-    description: 'Created By airwin',
-}
+    title: "Better DoneDeal Frontend",
+    description: "Created By airwin",
+};
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <head>
-            <ColorSchemeScript/>
-        </head>
-        <body className={inter.className}>
-        <MantineProvider>
-            {children}
-        </MantineProvider>
-        </body>
+            <head>
+                <ColorSchemeScript />
+            </head>
+            <body className={inter.className}>
+                <MantineProvider>{children}</MantineProvider>
+            </body>
         </html>
-    )
+    );
 }
